b51c23c78425b8fee896d1f2f7b00c178a852b65
Vanessa Ating <atingenkay@gmail.com>
2019-12-22 03:06:02 +0100

    set up test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders without crashing', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText('Learn React')).toBeInTheDocument();
 });
