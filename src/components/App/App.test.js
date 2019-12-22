3a0784ce3d1b47df8b0dbe92ea59bbdbd1f949c7
Vanessa Ating <atingenkay@gmail.com>
2019-12-22 04:35:56 +0100

    add travis config file

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,6 +3,9 @@ import { render } from '@testing-library/react';
 import App from '.';
 
 it('renders without crashing', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Learn React')).toBeInTheDocument();
+  const { container } = render(<App />);
+  expect(container.firstChild.classList.contains('navbar-container')).toBe(
+    true
+  );
+  expect(container).toHaveTextContent('Friday Code Challenge');
 });
