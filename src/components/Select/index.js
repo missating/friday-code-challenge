7ba65e60307936b7677e2b36be56a07df7eda0e9
Vanessa Ating <atingenkay@gmail.com>
2019-12-22 05:34:54 +0100

    add select component

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -2,11 +2,16 @@
 import React from 'react';
 
 // styles
+import './Select.scss';
 
 const Select = () => {
   return (
     <div className='select-container'>
-      <p>here</p>
+      <select className='select-container__content'>
+        <option>Select Car</option>
+        <option>No external background image</option>
+        <option>No wrapper</option>
+      </select>
     </div>
   );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // components
-import Navbar from '../components/Navbar';
 
 // pages
 import Home from '../pages/Home';
 
 const Routes = () => (
   <Router>
-    <>
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
-    </>
+    <Switch>
+      <Route exact path='/' component={Home} />
+    </Switch>
   </Router>
 );
 
